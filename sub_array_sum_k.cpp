17219f47afb9a7023f4617d679be9234c442c2e0
atharvp <atharvep@gmail.com>
2025-07-14 20:06:06 +0530

    Sub Array Sub =k Problem using Hashmap and prefix sum

---
+++ sub_array_sum_k.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<vector>
+#include<unordered_map>
+using namespace std;
+int main(){
+    vector<int>arr={1,2,3};
+    int n=arr.size();
+    int count=0;
+    vector<int>pre_fix(n);
+    unordered_map<int,int>map;
+    pre_fix[0]=arr[0];    
+    int k=3;
+    for(int i=1;i<n;i++){
+        pre_fix[i]=pre_fix[i-1]+arr[i];
+    }
+    for(int i=0;i<n;i++){
+        if(pre_fix[i]==k){
+            count++;
+        }
+        int num=pre_fix[i]-k;
+        if(map.find(num)!=map.end()){
+            count += map[num];
+        }
+        if(map.find(pre_fix[i])==map.end()){
+           map[pre_fix[i]]=0;
+
+        }
+        map[pre_fix[i]]++;
+    }
+    cout<<count;
+    
+   
+}

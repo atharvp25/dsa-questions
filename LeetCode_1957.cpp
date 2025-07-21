eb6b0943a67f8ae57c24ce888fc7063c50689cf2
Atharv P <145008300+atharvp25@users.noreply.github.com>
2025-07-22 00:18:56 +0530

    Create LeetCode_1957.cpp

---
+++ LeetCode_1957.cpp
@@ -0,0 +1,23 @@
+//Leetcode 1957. Delete Characters to Make Fancy String
+class Solution {
+public:
+    string makeFancyString(string s) {
+        string result;
+        int count = 1;
+
+        for (int i = 0; i < s.size(); ++i) {
+            if (i > 0 && s[i] == s[i - 1]) {
+                count++;
+            } else {
+                count = 1;
+            }
+
+            if (count < 3) {
+                result += s[i];
+            }
+        }
+
+        return result;
+        
+    }
+};

73437b5e29307f5f5fe2e0efb420789393739fa0
Atharv Pandav <145008300+atharvp25@users.noreply.github.com>
2025-08-14 19:48:33 +0530

    Create LeetCode_34.cpp

---
+++ LeetCode_34.cpp
@@ -0,0 +1,47 @@
+//Use Modify Binary Search 
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        vector<int>arr={-1,-1};
+
+        int left=0,right=nums.size()-1;
+
+        
+        while(left<=right){
+            int mid=left+(right-left)/2;
+
+            if(nums[mid]<target){
+                left=mid+1;
+            }
+            else{
+                if(nums[mid]==target){
+                    arr[0]=mid;
+                    
+                   
+                }
+                right=mid-1;
+
+            }
+        }
+        left=0;right=nums.size()-1;
+        while(left<=right){
+            int mid=left+(right-left)/2;
+
+            if(nums[mid]>target){
+                right=mid-1;
+            }
+            else{
+                if(nums[mid]==target){
+                    arr[1]=mid;                 
+                   
+                }
+                 left=mid+1;
+
+            }
+        }
+      
+        return arr;
+
+        
+    }
+};

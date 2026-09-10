82234f382dc9755b9abea8ee92134f4cb544f51c
Atharv Pandav <145008300+atharvp25@users.noreply.github.com>
2026-09-10 23:39:34 +0530

    Time: 5 ms (72.11%) | Memory: 15.8 MB (29.76%) - LeetSync

--- 2347-count-nodes-equal-to-average-of-subtree/count-nodes-equal-to-average-of-subtree.cpp
+++ 2347-count-nodes-equal-to-average-of-subtree/count-nodes-equal-to-average-of-subtree.cpp
@@ -1,39 +1,66 @@
+// class Solution {
+// public:
+
+//     int sum(TreeNode* root) {
+//         if (root == nullptr) {
+//             return 0;
+//         }
+
+//         return root->val + sum(root->left) + sum(root->right);
+//     }
+
+//     int counter(TreeNode* root) {
+//         if (root == nullptr) {
+//             return 0;
+//         }
+
+//         return 1 + counter(root->left) + counter(root->right);
+//     }
+
+//     int averageOfSubtree(TreeNode* root) {
+//         if (root == nullptr) {
+//             return 0;
+//         }
+
+//         int Sum = sum(root);
+//         int count = counter(root);
+
+//         int ans = 0;
+
+//         if (Sum / count == root->val) {
+//             ans = 1;
+//         }
+
+//         ans += averageOfSubtree(root->left);
+//         ans += averageOfSubtree(root->right);
+
+//         return ans;
+//     }
+// };
 class Solution {
 public:
+    int ans = 0;
 
-    int sum(TreeNode* root) {
+    pair<int, int> dfs(TreeNode* root) {
         if (root == nullptr) {
-            return 0;
+            return {0, 0};
         }
 
-        return root->val + sum(root->left) + sum(root->right);
-    }
+        auto left = dfs(root->left);
+        auto right = dfs(root->right);
 
-    int counter(TreeNode* root) {
-        if (root == nullptr) {
-            return 0;
+        int sum = left.first + right.first + root->val;
+        int count = left.second + right.second + 1;
+
+        if (sum / count == root->val) {
+            ans++;
         }
 
-        return 1 + counter(root->left) + counter(root->right);
+        return {sum, count};
     }
 
     int averageOfSubtree(TreeNode* root) {
-        if (root == nullptr) {
-            return 0;
-        }
-
-        int Sum = sum(root);
-        int count = counter(root);
-
-        int ans = 0;
-
-        if (Sum / count == root->val) {
-            ans = 1;
-        }
-
-        ans += averageOfSubtree(root->left);
-        ans += averageOfSubtree(root->right);
-
+        dfs(root);
         return ans;
     }
 };

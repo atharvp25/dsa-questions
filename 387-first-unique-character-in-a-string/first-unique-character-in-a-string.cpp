ff40e826211e484428a043e6cdc29c481090e5cb
Atharv Pandav <145008300+atharvp25@users.noreply.github.com>
2026-09-13 22:22:03 +0530

    Time: 2 ms (82.48%) | Memory: 14.6 MB (86.96%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
@@ -1,26 +1,46 @@
+// class Solution {
+// public:
+//     int firstUniqChar(string s) {
+//         unordered_map<char,int>map1;
+//         unordered_map<char,int>map2;
+
+//         for(int i=0;i<s.length();i++){
+//             map1[s[i]]++;
+//             map2[s[i]]=i;
+//         }
+//         vector<char>ch;
+//         for(auto p : map1){
+//             if(p.second==1){
+//                 ch.push_back(p.first);
+//             }
+//         }
+//         int ans=INT_MAX;
+//         for(int i=0;i<ch.size();i++){
+//             ans=min(ans,map2[ch[i]]);
+//         }
+//         if(ans==INT_MAX){
+//             return -1;
+//         }
+//         return ans;
+//     }
+// };
 class Solution {
 public:
     int firstUniqChar(string s) {
-        unordered_map<char,int>map1;
-        unordered_map<char,int>map2;
+        int freq[26] = {0};
 
-        for(int i=0;i<s.length();i++){
-            map1[s[i]]++;
-            map2[s[i]]=i;
+        // Count frequency
+        for (char c : s) {
+            freq[c - 'a']++;
         }
-        vector<char>ch;
-        for(auto p : map1){
-            if(p.second==1){
-                ch.push_back(p.first);
+
+        // Find first character whose frequency is 1
+        for (int i = 0; i < s.length(); i++) {
+            if (freq[s[i] - 'a'] == 1) {
+                return i;
             }
         }
-        int ans=INT_MAX;
-        for(int i=0;i<ch.size();i++){
-            ans=min(ans,map2[ch[i]]);
-        }
-        if(ans==INT_MAX){
-            return -1;
-        }
-        return ans;
+
+        return -1;
     }
 };
